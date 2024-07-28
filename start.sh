886a70ea56c6878cd91c14d85fc3147f660f10f5
juacrumar <juacrumar@lairen.eu>
2024-07-28 14:46:25 +0200

    use a different curl call for fine grained tokens

--- start.sh
+++ start.sh
@@ -16,15 +16,26 @@ source /home/docker/.ACCESS_TOKEN
 # ACCESS_TOKEN="******************"
 
 # REGISTRATION TOKEN
-
+echo $ACCES
 # Obtain the REG_TOKEN via post request to the github api
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+if [[ $ACCESS_TOKEN = *"github_"* ]]
+then
+    ## If using instead fine-grained tokens:
+    REG_TOKEN=$(curl -L \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer ${ACCESS_TOKEN}" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+else
+    REG_TOKEN=$(curl -sX POST -H "AuthorizaTion: token ${ACCESS_TOKEN}" https://api.github.com/repos/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+fi
+
 # Alternatively the REG_TOKEN can be written explicitly here
 # !WARNING: DO NOT COMMIT A PLAIN TOKEN!
 # REG_TOKEN="**************"
 
 # MAIN SCRIPT
-
 cd /home/docker/actions-runner
 
 ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
